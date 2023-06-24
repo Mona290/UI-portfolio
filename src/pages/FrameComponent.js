a6da21f3505ce387b9109fc59f35f9ba6c0282c0
Mona Rai <94286462+Mona290@users.noreply.github.com>
2023-06-24 23:43:23 +0530

    Add files via upload

---
+++ src/pages/FrameComponent.js
@@ -0,0 +1,242 @@
+import "./FrameComponent.css";
+const FrameComponent = () => {
+  return (
+    <div className="component-1-parent">
+      <div className="component-1">
+        <div className="phone-no70xxxxxxxx-email-moxx-container">
+          <p className="phone-no70xxxxxxxx">
+            <span className="phone-no">phone no</span>
+            <span className="span">.</span>
+            <span>70xxxxxxxx</span>
+          </p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="email-moxxxxgmailcom">
+            <span className="email">Email</span>
+            <span className="span">-</span>
+            <span>moxxxx@gmail.com</span>
+          </p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="address-bihar844101">
+            <span className="address">Address</span>
+            <span className="span">-</span>
+            <span>Bihar,844101</span>
+            <span className="span">,</span>
+          </p>
+        </div>
+        <div className="httpsgithubcommona290">https://github.com/Mona290</div>
+        <div className="linkedin">LinkedIn</div>
+        <div className="a-sleek-weather-container">
+          <p className="blank-line">
+            A sleek Weather App designed with a focus on user interface (UI).
+            This app provides up-to-date weather information in a visually
+            appealing and intuitive format.With a minimalist and modern design,
+            the Weather App offers a clean and uncluttered interface. The use of
+            vibrant colors and smooth transitions enhances the overall user
+            experience, making it visually engaging and enjoyable to navigate.
+          </p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="blank-line">
+            The app provides real-time weather data, including temperature,
+            humidity, wind speed, and forecast for multiple locations. Users can
+            easily switch between locations and view the weather information
+            that matters to them. The intuitive UI ensures that users can access
+            the desired information quickly and effortlessly.
+          </p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="blank-line">
+            To further enhance the user experience, the Weather App utilizes
+            animations and graphical representations to depict weather
+            conditions. Users can easily grasp the current weather status at a
+            glance, making it convenient for planning activities and making
+            informed decisions.
+          </p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="blank-line">
+            Additionally, the app offers customizable features, allowing users
+            to personalize their experience. They can set preferences, such as
+            temperature units (Celsius or Fahrenheit) and choose from different
+            themes to suit their style and preferences.
+          </p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="blank-line">
+            The Weather App is designed to be responsive and compatible with
+            various devices, ensuring a seamless experience across smartphones,
+            tablets, and desktops. It takes advantage of the latest technologies
+            to deliver accurate and reliable weather information.
+          </p>
+          <p className="blank-line6">&nbsp;</p>
+        </div>
+        <div className="introducing-my-latest">
+          Introducing my latest project: a captivating Restaurant Website that
+          prioritizes UI/UX design. It offers an immersive experience,
+          showcasing the restaurant's unique offerings. The interface is
+          visually appealing, with intuitive navigation for easy exploration.
+          The website is responsive and works seamlessly across devices. It
+          includes features like online reservations and ordering for enhanced
+          convenience. Overall, it creates a virtual gateway to the restaurant,
+          enticing visitors to indulge in its culinary delights.
+        </div>
+        <div className="restaurant-website">Restaurant Website</div>
+        <div className="projects">Projects</div>
+        <img className="component-1-child" alt="" src="/ellipse-4@2x.png" />
+        <div className="presentation">Presentation</div>
+        <div className="every-problem-is">
+          {" "}
+          "Every problem is an opportunity in disguise." - John Adams
+        </div>
+        <div className="text">{`  `}</div>
+        <div className="about">ABOUT</div>
+        <div className="effective-prioritization-is">
+          "Effective prioritization is the art of saying no to good
+          opportunities in order to say yes to great ones."
+        </div>
+        <img className="component-1-item" alt="" src="/ellipse-3@2x.png" />
+        <div className="prioritization">Prioritization</div>
+        <div className="effective-communication-is">
+          "Effective communication is the bridge that connects understanding and
+          success."
+        </div>
+        <div className="collaboration-is-the">
+          "Collaboration is the key to unlocking collective genius." - Whitney
+          Johnson
+        </div>
+        <div className="power-skills">Power Skills</div>
+        <div className="adobe">2.Adobe</div>
+        <img className="component-1-inner" alt="" src="/rectangle-9@2x.png" />
+        <div className="figma">1.Figma</div>
+        <div className="design-tools">Design Tools</div>
+        <div className="user-interface-design-container">
+          <p className="blank-line">1.User Interface</p>
+          <p className="blank-line">Design</p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="blank-line">2.User Experience Design</p>
+        </div>
+        <img className="polygon-icon" alt="" src="/polygon-1@2x.png" />
+        <div className="problem-solver">
+          <p className="blank-line">Problem</p>
+          <p className="blank-line">Solver</p>
+        </div>
+        <div className="skills">SKILLS</div>
+        <img className="ellipse-icon" alt="" src="/ellipse-1@2x.png" />
+        <div className="education">EDUCATION</div>
+        <div className="i-am-mona-container">
+          <p className="i-am-mona-rai">
+            <span>
+              <span>
+                <span className="i-am">I am</span>
+              </span>
+              <span>
+                <span> MONA RAI.</span>
+              </span>
+            </span>
+          </p>
+          <p className="web-designer">
+            <span>
+              <span>
+                <span className="web-designer1">Web Designer</span>
+              </span>
+            </span>
+          </p>
+          <p className="blank-line8">
+            <span>
+              <span>
+                <span>&nbsp;</span>
+              </span>
+            </span>
+          </p>
+          <p className="to-secure-a-challenging-positi">
+            <span>
+              <span>
+                <span>
+                  To secure a challenging position as a UI/UX Developer in a
+                  dynamic organization where I can apply my creative design
+                  skills and technical expertise to deliver intuitive and
+                  visually appealing user experiences.I aim to collaborate with
+                  cross-functional teams, leveraging my strong problem-solving
+                  abilities and attention to detail, to create innovative
+                  digital solutions that enhance user engagement, satisfaction,
+                  and overall product success. By continuously learning and
+                  adapting to emerging design trends and technologies, I strive
+                  to contribute to the growth of the organization while honing
+                  my skills as a UI/UX professional.
+                </span>
+              </span>
+            </span>
+          </p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="blank-line">&nbsp;</p>
+          <p className="blank-line">&nbsp;</p>
+        </div>
+        <img className="rectangle-icon" alt="" src="/rectangle-2@2x.png" />
+        <img className="component-1-child1" alt="" src="/rectangle-3@2x.png" />
+        <img className="component-1-child2" alt="" src="/rectangle-4@2x.png" />
+        <div className="i-am-currently">
+          I am currently pursuing a Bachelor's degree in Technology,
+          specializing in Computer Science and Engineering, from Lovely
+          Professional University (LPU). As a dedicated student, I am actively
+          engaged in acquiring a comprehensive understanding of various
+          programming languages, software development methodologies, and
+          cutting-edge technologies relevant to the field.
+        </div>
+        <div className="i-successfully-completed">
+          I successfully completed my 10th-grade education from the Central
+          Board of Secondary Education (CBSE) board, securing an impressive
+          score of 84%. This formative period allowed me to develop a
+          disciplined approach to learning and laid the groundwork for my
+          subsequent academic endeavors.
+        </div>
+        <img className="component-1-child3" alt="" src="/rectangle-5@2x.png" />
+        <div className="prior-to-my">
+          Prior to my current academic pursuit, I completed my Intermediate
+          education from a state board, where I achieved a commendable score of
+          74.4%. This phase of my education provided me with a strong foundation
+          in subjects related to science and mathematics, further fueling my
+          passion for technology and problem-solving.
+        </div>
+        <img className="component-1-child4" alt="" src="/rectangle-7@2x.png" />
+        <img className="component-1-child5" alt="" src="/rectangle-8@2x.png" />
+        <img className="component-1-child6" alt="" src="/rectangle-10@2x.png" />
+        <img className="component-1-child7" alt="" src="/ellipse-2@2x.png" />
+        <div className="communication">Communication</div>
+        <img className="component-1-child8" alt="" src="/rectangle-11@2x.png" />
+        <img className="component-1-child9" alt="" src="/rectangle-12@2x.png" />
+        <img
+          className="component-1-child10"
+          alt=""
+          src="/rectangle-13@2x.png"
+        />
+        <img
+          className="component-1-child11"
+          alt=""
+          src="/rectangle-14@2x.png"
+        />
+        <img
+          className="component-1-child12"
+          alt=""
+          src="/rectangle-15@2x.png"
+        />
+        <div className="mask-group">
+          <img className="mask-group-child" alt="" src="/rectangle-16@2x.png" />
+        </div>
+        <img
+          className="component-1-child13"
+          alt=""
+          src="/rectangle-17@2x.png"
+        />
+        <div className="weather-app-ui">Weather App UI Design</div>
+        <div className="personal-portfolio">
+          <div className="personal-portfolio1">PERSONAL PORTFOLIO</div>
+        </div>
+        <div className="personal-portfolio2">Personal Portfolio</div>
+        <img
+          className="component-1-child14"
+          alt=""
+          src="/rectangle-18@2x.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FrameComponent;
